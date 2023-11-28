2854a974debde857dd1d9143948176dcf95c1fc2
Rishi Gandhi <rishigandhi2002@gmail.com>
2023-11-28 19:27:09 -0330

    button text size changes dynamically

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ root = tk.Tk()
 root.title("Calculator")
 
 # Entry widget to display the input and output
-entry = tk.Entry(root, font=("Helvetica", 24), justify="right")
+entry = tk.Entry(root, font=("Helvetica", 32), justify="right")
 entry.grid(row=0, column=0, columnspan=4, sticky="nsew")
 
 # Button layout
@@ -42,6 +42,10 @@ col_val = 0
 for button_text in buttons:
     button = tk.Button(root, text=button_text, width=4, height=2, command=create_button_handler(button_text))
     button.grid(row=row_val, column=col_val, sticky="nsew")
+    
+    # Bind the <Configure> event to adjust font size dynamically
+    button.bind("<Configure>", lambda event, button=button: adjust_button_font_size(event, button))
+    
     col_val += 1
     if col_val > 3:
         col_val = 0
@@ -51,6 +55,11 @@ for button_text in buttons:
 for i in range(4):
     root.columnconfigure(i, weight=1)
     root.rowconfigure(i + 1, weight=1)
+
+# Function to adjust button font size based on button size
+def adjust_button_font_size(event, button):
+    new_font_size = max(10, int(event.width / 10))
+    button['font'] = ('Helvetica', new_font_size)
 
 # Run the main loop
 root.mainloop()
